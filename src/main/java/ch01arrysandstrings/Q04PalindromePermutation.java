24951969b9dee34a4a45eb6cea79b6c263251ed4
trpfg93 <shubo_zhang@troweprice.com>
2018-03-28 10:06:05 -0400

    Completed Q04 PalindromePermutation

--- src/main/java/ch01arrysandstrings/Q04PalindromePermutation.java
+++ src/main/java/ch01arrysandstrings/Q04PalindromePermutation.java
@@ -4,7 +4,43 @@ public class Q04PalindromePermutation {
 
     public static boolean isPalindromePermutation(String s){
         if (s == null || s.length() == 0) { return false; }
-        if (s.length() == 1) { return true; }
-        return false;
+        if (s.length() == 1 || s.length() == 2) { return true; }
+
+        s = s.toLowerCase(); // Assumption: case insensitive
+        int[] codes = new int[128]; // Assumption: all ascii letters
+
+        int letterCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int code = s.charAt(i);
+            if (code < 97 || code > 122) { continue; } // ignore non-letter
+            codes[code]++;
+            letterCount++;
+        }
+        boolean allowOdd = letterCount % 2 != 0 ? true : false;
+        for (int i = 0; i < 128; i++) {
+            if (codes[i] == 0) { continue;}
+            if (codes[i] % 2 != 0){
+                if (allowOdd){
+                    allowOdd = false;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Alternative solution: bit operation
+
+
+    public static void main(String[] args) {
+        System.out.println(isPalindromePermutation(null) == false);
+        System.out.println(isPalindromePermutation("") == false);
+        System.out.println(isPalindromePermutation("a") == true);
+        System.out.println(isPalindromePermutation("a ") == true);
+        System.out.println(isPalindromePermutation("aa") == true);
+        System.out.println(isPalindromePermutation("ab") == true);
+        System.out.println(isPalindromePermutation("accc") == false);
+        System.out.println(isPalindromePermutation("Tact Coa") == true);
     }
 }
